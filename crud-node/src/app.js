51486573c5debddbca5333e8a6076bcc36f1a1e8
JeffreyRios24 <jeffrey.rios@correounivalle.edu.co>
2019-04-24 11:52:53 -0500

    Creacion de modulos y servidor

---
+++ crud-node/src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+//settings
+app.set('port', process.env.PORT || 3000);
+
+
+app.listen(3000, () =>{
+	console.log('Server on port 3000');
+});
