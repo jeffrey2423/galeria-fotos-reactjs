14cf93bb5296f5d0755794a12dd3cb5dbeaaf1e9
JeffreyRios24 <jeffrey.rios@correounivalle.edu.co>
2019-04-29 17:51:39 -0500

    barra de navegacion y formulario

--- app-web-node/src/index.js
+++ app-web-node/src/index.js
@@ -33,7 +33,7 @@ app.use((req, res, next) =>{
 //rutas
 app.use(require('./routes/'));
 app.use(require('./routes/authentication'));
-app.use('links',require('./routes/links'));
+app.use('/links',require('./routes/links'));
 
 //publico
 app.use(express.static(path.join(__dirname, 'public')));
--- app-web-node/src/routes/links.js
+++ app-web-node/src/routes/links.js
@@ -3,4 +3,20 @@ const router = express.Router();
 
 const pool = require('../database');
 
+router.get('/add', (req, res) =>{
+	res.render('links/add');
+});
+
+router.post('/add', async (req, res) =>{
+	const {title, url, description} = req.body;
+	const newLink = {
+		title,
+		url,
+		description
+	};
+	await pool.query('INSERT INTO links set ?', [newLink]);
+
+	res.send('recibido');
+});
+
 module.exports = router;
