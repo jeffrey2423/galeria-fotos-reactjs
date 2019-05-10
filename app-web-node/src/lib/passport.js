ec469bc27f46d97d4177679df9d22d41a2c0a3b9
JeffreyRios24 <37357913+jeffrey2423@users.noreply.github.com>
2019-05-10 17:55:37 -0500

    encriptando contraseña con passport

--- app-web-node/src/lib/passport.js
+++ app-web-node/src/lib/passport.js
@@ -2,6 +2,7 @@ const passport = require('passport');
 const localStrategy = require('passport-local').Strategy;
 
 const pool = require('../database');
+const helpers = require('../lib/helpers');
 
 passport.use('local.signup', new localStrategy({
 	usernameField: 'username',
@@ -12,17 +13,32 @@ passport.use('local.signup', new localStrategy({
 }, async(req, username, password, done)=>{
 	const {fullname} = req.body;
 
-	const newUser{
+	const newUser = {
 		username,
 		password,
 		fullname
 	};
-	await pool.query('INSERT INTO users SET ?', [newUser]);
+	//encriptamos la contraseña
+	newUser.password = await helpers.encryptPassword(password);
+	//validamos que el usuario no sea el mismo
+	const verUser = await pool.query('SELECT count(*) FROM users Where username = ?', [username]);
+	const resultUser = mysql_fetch_row(verUser);
+	if(resultUser === 0){
+			const result = await pool.query('INSERT INTO users SET ?', [newUser]);
+			newUser.id = result.insertId;
+			return done(null, newUser);
+	}else{
+		req.flash('success', 'Link guardado correctamente');
+	}
 
 }));
 
-/*passport.serializeUser((user, done) =>{
+passport.serializeUser((user, done) =>{
+	done(null, user.id);
+});
 
+passport.deserializeUser(async (id, done) =>{
+	const rows = await pool.query('SELECT * FROM users Where id = ?', [id]);
+	done(null, rows[0]);
+});
 
-
-});*/
