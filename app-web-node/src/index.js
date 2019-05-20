cd1c6a8b5e302c2875c81541d73338c0cd8b5302
JeffreyRios24 <jeffrey.rios@correounivalle.edu.co>
2019-05-20 14:57:25 -0500

    Update index.js

--- app-web-node/src/index.js
+++ app-web-node/src/index.js
@@ -27,6 +27,20 @@ app.engine('.hbs', exphbs({
 }));
 app.set('view engine', '.hbs');
 
+app.use((req, res, next) => {
+
+// Dominio que tengan acceso (ej. 'http://example.com')
+   res.setHeader('Access-Control-Allow-Origin', '*');
+
+// Metodos de solicitud que deseas permitir
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+
+// Encabecedados que permites (ej. 'X-Requested-With,content-type')
+   res.setHeader('Access-Control-Allow-Headers', '*');
+
+next();
+})
+
 app.use(session({
 	secret: 'sessionapp',
   	resave: false,
