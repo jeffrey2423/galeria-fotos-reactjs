c4ea0b0b5df1298584c6ce5cbc1eacfbef5de912
JeffreyRios24 <jeffrey.rios@correounivalle.edu.co>
2019-07-05 10:57:52 -0500

    Aplicacion terminada

--- react-apps/react-pixabay/src/App.js
+++ react-apps/react-pixabay/src/App.js
@@ -1,20 +1,72 @@
 import React, {Component} from 'react';
 import Buscador from './componentes/Buscador';
-
+import Resultado from './componentes/Resultado';
+//import Imagen from './componentes/Imagen';
 class App extends Component {
 
   state = {
-    termino :""
+    termino :"",
+    imagenes :[],
+    pagina: ""
+  }
+
+  scroll = () =>{
+    const elemento = document.querySelector('.jumbotron');
+    elemento.scrollIntoView('smoot', 'start');
+  }
+
+  paginaAnterior = () =>{
+    //leer state pagina actual
+    let pagina = this.state.pagina;
+
+    //validar si la pagina es uno
+    if(pagina === 1) return null;
+
+    //restar uno a la pagina actual
+    pagina--;
+
+    // agregar cambio al state
+    this.setState({
+      pagina
+    }, () => {
+      this.consultarApi();
+      this.scroll();
+    });
+
+    console.log(pagina);
+  }
+  paginaSiguiente = () =>{
+    //leer state pagina actual
+    let pagina = this.state.pagina;
+
+    //sumar uno a la pagina actual
+    pagina++;
+
+    // agregar cambio al state
+    this.setState({
+      pagina
+    }, () => {
+      this.consultarApi();
+      this.scroll();
+    });
+
+    console.log(pagina);
   }
 
   consultarApi = () =>{
-    const url ='https://pixabay.com/api/?key=11865265-50cb1a9338b6b50f08250dc6b&q=${}';
-    console.log(url);
+    const pagina = this.state.pagina;
+    const url =`https://pixabay.com/api/?key=11865265-50cb1a9338b6b50f08250dc6b&q=${this.state.termino}&per_page=30&&page=${pagina}`;
+    
+    //console.log(url);
+    fetch(url)
+    .then(respuesta => respuesta.json())
+    .then(resultado => this.setState({imagenes: resultado.hits}))
   }
 
   datosBusqueda = (termino) =>{
     this.setState({
-      termino
+      termino: termino,
+      pagina: 1
     }, () =>{
       this.consultarApi();
     })
@@ -28,7 +80,13 @@ class App extends Component {
               datosBusqueda={this.datosBusqueda}
             />
           </div>
-          {this.state.termino}
+          <div className="row justify-content-center">
+            <Resultado
+            imagenes={this.state.imagenes}
+            paginaAnterior={this.paginaAnterior}
+            paginaSiguiente={this.paginaSiguiente}
+            />
+          </div>
         </div>
     );
   }
